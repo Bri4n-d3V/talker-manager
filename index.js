a2296ac80947690a45878387a91c5ad00030302e
BrianCaetano <brian-caetano@hotmail.com>
2022-01-14 19:33:01 -0300

    Req 4

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getAllTalkers } = require('./middlewares/getAllTalkers');
+const {
+  getAllTalkers,
+} = require('./middlewares/getAllTalkers');
 const getTalkerById = require('./middlewares/getTalkerById');
-const { handleEmail, handlePassword, handleLogin } = require('./middlewares/login');
+const {
+  handleEmail,
+  handlePassword,
+  handleLogin,
+} = require('./middlewares/login');
+const {
+  handleToken,
+  handleName,
+  handleAge,
+  handleWatchedAt,
+  handleRate,
+  handleTalk,
+  createTalk,
+} = require('./middlewares/createTalker');
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,6 +33,11 @@ app.get('/', (_request, response) => {
 app.get('/talker', getAllTalkers);
 app.get('/talker/:id', getTalkerById);
 app.post('/login', handleEmail, handlePassword, handleLogin);
+app.post(
+  '/talker',
+  handleToken, handleName, handleAge,
+  handleTalk, handleWatchedAt, handleRate, createTalk,
+);
 
 app.listen(PORT, () => {
   console.log('Online');
