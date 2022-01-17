6506c3a31dedae5595eed9c892d9078ebcf2f49c
BrianCaetano <brian-caetano@hotmail.com>
2022-01-17 16:39:50 -0300

    Refatora o todo o resto com router eeeee

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// const getTalkerById = require('./middlewares/getTalkerById');
-const {
-  handleToken,
-  handleName,
-  handleAge,
-  handleWatchedAt,
-  handleRate,
-  handleTalk,
-  createTalk,
-} = require('./middlewares/createTalker');
-const { editTalker } = require('./middlewares/editTalker');
-const { deleteTalker } = require('./middlewares/deleteTalker');
-const searchTalker = require('./middlewares/searchTalker');
-
 const talkerRoute = require('./routes/talker');
 const loginRoute = require('./routes/login');
 
@@ -29,23 +15,8 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker/search', handleToken, searchTalker);
-
 app.use('/talker', talkerRoute);
 app.use('/login', loginRoute);
-
-// app.get('/talker/:id', getTalkerById);
-app.post(
-  '/talker',
-  handleToken, handleName, handleAge,
-  handleTalk, handleWatchedAt, handleRate, createTalk,
-);
-app.put(
-  '/talker/:id',
-  handleToken, handleName, handleAge,
-  handleTalk, handleWatchedAt, handleRate, editTalker,
-);
-app.delete('/talker/:id', handleToken, deleteTalker);
 
 app.listen(PORT, () => {
   console.log('Online');
--- routes/talker.js
+++ routes/talker.js
@@ -5,9 +5,31 @@ const router = express.Router();
 const {
   getAllTalkers,
 } = require('../middlewares/getAllTalkers');
+
 const getTalkerById = require('../middlewares/getTalkerById');
 
+const {
+  handleToken,
+  handleName,
+  handleAge,
+  handleWatchedAt,
+  handleRate,
+  handleTalk,
+  createTalk,
+} = require('../middlewares/createTalker');
+
+const { editTalker } = require('../middlewares/editTalker');
+
+const { deleteTalker } = require('../middlewares/deleteTalker');
+const searchTalker = require('../middlewares/searchTalker');
+
+router.get('/search', handleToken, searchTalker);
 router.get('/', getAllTalkers);
 router.get('/:id', getTalkerById);
+router.post('/', handleToken, handleName, handleAge,
+handleTalk, handleWatchedAt, handleRate, createTalk); 
+router.put('/:id', handleToken, handleName, handleAge,
+handleTalk, handleWatchedAt, handleRate, editTalker);
+router.delete('/:id', handleToken, deleteTalker);
 
 module.exports = router;
